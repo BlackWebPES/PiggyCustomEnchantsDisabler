29965abb36c605cc0c5a9d44004578b9ac1177e4
Saini14 TV <38633228+Saini14@users.noreply.github.com>
2018-07-31 22:07:09 -0700

    To stop the dcma for taking a toll...

--- src/saiini/TV/Main.php
+++ src/saiini/TV/Main.php
@@ -1,14 +1,16 @@
 <?php
+
+/// ,-.                 ,   ,. 
+//(   `     o o     o '|  / | 
+// `-.  ,-: . . ;-. .  | '--| 
+//.   ) | | | | | | |  |    | 							| Always on it|
+// `-'  `-` ' ' ' ' '  '    '
 declare(strict_types = 1);
 namespace saiini\TV;
 use PiggyCustomEnchants\Main as CE;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\config;
 use pocketmine\event\Listener;
-/**
- * S a i n i 1 4
- */
-
 /**
  * Class Main
  *
@@ -17,16 +19,27 @@ use pocketmine\event\Listener;
 class Main extends PluginBase implements Listener{
 	/**@var config */
 	public $cfg;
-	public function onEnable(){
-		if(!is_dir($this->getDataFolder())){
+	public function onEnable()
+	{
+		if (!is_dir($this->getDataFolder())) {
 			mkdir($this->getDataFolder());
+		}
+		if (!mkdir($this->getDataFolder())) {
+			$this->getLogger()->info("cannot load the config file for a strange reason...");
 		}
 		$this->saveDefaultConfig();
 		/** @var CE $ce */
+		if($ce == 0){
+			$this->getLogger()->alert("And odd error appeared...");
+		}
 		$ce = $this->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
 		$ids = $this->getConfig()->get("ids");
-		foreach($ids as $id){
+		foreach ($ids as $id) {
 			$ce->unregisterEnchantment($id);
+			$this->getLogger()->info('Disabled: ' . $id);
+			if (!$id) {
+				$this->getLogger()->info('No Enchantment found.');
+			}
 		}
 	}
-}
+
